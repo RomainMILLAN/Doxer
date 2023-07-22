24bb70d0eda52b4af217d394157d4de705d8d193
Fryzhen <100153667+Fryzhen@users.noreply.github.com>
2023-07-22 13:29:55 +0200

    Update PollExclamerMessages.java

--- src/main/java/fr/skytorstd/doxer/states/messages/plugin/PollExclamerMessages.java
+++ src/main/java/fr/skytorstd/doxer/states/messages/plugin/PollExclamerMessages.java
@@ -2,16 +2,16 @@ package fr.skytorstd.doxer.states.messages.plugin;
 
 public enum PollExclamerMessages {
 
-    PLUGIN_DESCRIPTION("Plugin permettant de crée des sondages rapidement"),
+    PLUGIN_DESCRIPTION("Plugin permettant de créer des sondages rapidement"),
 
     PLUGIN_COMMAND_POLL("/poll <question> <:emoji1:, :emoji2:, ...>"),
-    PLUGIN_COMMAND_POLL_DESCRIPTION("Commande pour crée un sondage"),
+    PLUGIN_COMMAND_POLL_DESCRIPTION("Commande pour créer un sondage"),
     PLUGIN_OPTION_QUESTION_DESCRIPTION("Question du sondage"),
-    PLUGIN_OPTION_ANSWER_DESCRIPTION("Réponse sous formes d'émoji"),
+    PLUGIN_OPTION_ANSWER_DESCRIPTION("Réponses sous formes d'émojis"),
 
     POLL_OF("Sondage de"),
-    POLL_CREATED("Sondage crée"),
-    SENTRY_POLL_CREATED("Un sondage crée (`%s`)");
+    POLL_CREATED("Sondage créé"),
+    SENTRY_POLL_CREATED("Un sondage a été créé (`%s`)");
 
     private final String message;
 
